749e1403be24293bb49d162a2be5fd83c6002f82
Carlos Daniel <carlosdanieldev@gmail.com>
2026-06-10 23:20:17 -0300

    chore: create font extension to use it across application only with struct `Font`

---
+++ qrsafe/Font+Extension.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+extension Font {
+    static var qsVerdict: Font { QSTypography.verdict }
+    static var qsTitle: Font { QSTypography.title }
+    static var qsHeading: Font { QSTypography.heading }
+    static var qsBody: Font { QSTypography.body }
+    static var qsSecondary: Font { QSTypography.secondary }
+    static var qsCaption: Font { QSTypography.caption }
+}
