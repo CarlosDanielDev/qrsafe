14f8049384335ac9869bc7118799ecc761dfa271
Carlos Daniel <carlosdanieldev@gmail.com>
2026-06-13 01:05:39 -0300

    feat: create app skeleton navigation pages

---
+++ qrsafe/presentation/views/ScannerView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        NavigationStack {
+        Text("Hello Scanner!")
+                .navigationTitle("Scanner")
+        }
+    }
+}
