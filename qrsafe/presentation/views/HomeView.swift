1bb3f9cd1dc127284e466c3921abba293862e697
Carlos Daniel <carlosdanieldev@gmail.com>
2026-06-13 11:55:36 -0300

    feat: add request permission to camera usage

--- qrsafe/presentation/views/HomeView.swift
+++ qrsafe/presentation/views/HomeView.swift
@@ -1,17 +1,41 @@
+import AVFoundation
 import SwiftUI
 
 struct HomeView: View {
+    var isAuthorized: Bool {
+        get async {
+            let status = AVCaptureDevice.authorizationStatus(for: .video)
+
+            var isAuthorized = status == .authorized
+
+            if status == .notDetermined {
+                isAuthorized = await AVCaptureDevice.requestAccess(for: .video)
+            }
+
+            return isAuthorized
+        }
+    }
+    
+    func setUpCaptureSession() async {
+        guard await isAuthorized else {
+            return
+        }
+    }
+
     var body: some View {
         TabView {
             Tab("Scan", systemImage: "qrcode.viewfinder") {
-               ScannerView()
+                ScannerView()
             }
             Tab("History", systemImage: "clock.arrow.circlepath") {
-               HistoryView()
+                HistoryView()
             }
             Tab("Settings", systemImage: "gearshape") {
-               SettingsView()
+                SettingsView()
             }
+        }
+        .task {
+            await setUpCaptureSession()
         }
     }
 }
